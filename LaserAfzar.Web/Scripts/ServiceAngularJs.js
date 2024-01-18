6c2a9d209c58f97d5f05ee7cfd579d2be1e4b66c
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-01-18 21:32:38 +0200

    Fix issues with date formatting

---
+++ LaserAfzar.Web/Scripts/ServiceAngularJs.js
@@ -0,0 +1,72 @@
+﻿'use strict';
+
+(function () {
+
+
+
+
+
+    var app = angular.module("LaserAfzarServiceViewer", []);
+
+    app.filter('split', function () {
+        return function (input, splitChar, splitIndex) {
+            // do some bounds checking here to ensure it has that index
+            return input.split(splitChar)[splitIndex];
+        }
+    });
+
+    app.directive('myPostRepeatDirective', function () {
+        return function (scope, element, attrs) {
+            if (scope.$last) {
+                // iteration is complete, do whatever post-processing
+                // is necessary
+                myFunc();
+            }
+        };
+    });
+
+    app.directive('myNextRepeatDirective', function () {
+        return {
+            link: function ($scope, element, attrs) {
+                // Trigger when number of children changes,
+                // including by directives like ng-repeat
+                var watch = $scope.$watch(function () {
+                    return element.children().length;
+                }, function () {
+                    // Wait for templates to render
+                    $scope.$evalAsync(function () {
+                        // Finally, directives are evaluated
+                        // and templates are renderer here
+                        //var children = element.children();
+                        //console.log(children);
+                        myNanoGalleryFunc();
+                    });
+                });
+            },
+        };
+    });
+
+    var ServiceViewerController = function ($scope, $http, $sce) {
+
+
+        var onComplete = function (response) {
+            $scope.ServiceDetail = response.data;
+            $scope.trustAsHtml = $sce.trustAsHtml;
+        };
+
+        var onError = function (reason) {
+            $scope.ServiceDetailsError = "There is an error within retreaving the information";
+        };
+
+        $scope.$watch("serviceId", function () {
+
+            $http.get("/api/ServicesApi/" + $scope.serviceId)
+                  .then(onComplete, onError);
+
+        });
+
+    };
+
+    app.controller("ServiceViewerController", ["$scope", "$http", "$sce", ServiceViewerController]);
+
+}());
