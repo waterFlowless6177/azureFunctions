c0d7241f1ca0af9711e1de126fb25bef39265902
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-02-02 21:32:38 +0200

    add new tests

---
+++ LaserAfzar.Web/Scripts/ContactScript.js
@@ -0,0 +1,32 @@
+﻿
+(function () {
+
+    $('#contactus-response-success').hide();
+
+    $('#contactForm').ajaxForm({
+        beforeSerialize: function () {
+
+        },
+        success: function (d) {
+
+        },
+        complete: function (xhr) {
+
+            var obj = jQuery.parseJSON(xhr.responseText);
+
+            if (obj.status == "Success") {
+                //hide contactus form
+                $('#contactForm').hide();
+                //show response form
+                $('#contactus-response-success').show();
+            }
+            else {
+                var errorMessage = "خطا در ارسال پیغام";
+                alert(errorMessage);
+            }
+
+        }
+    });
+
+
+})();
