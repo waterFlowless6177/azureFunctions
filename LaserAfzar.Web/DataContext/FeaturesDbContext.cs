052f39bce749747ef1edb978a2e203e34c67a800
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-03-27 21:32:38 +0200

    Improve logging for errors

---
+++ LaserAfzar.Web/DataContext/FeaturesDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LaserAfzar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LaserAfzar.Web.DataContext
+{
+    public class FeaturesDbContext : DbContext
+    {
+        public FeaturesDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Feature> Features { get; set; }
+    }
+}
