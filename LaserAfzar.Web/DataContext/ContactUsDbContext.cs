633c81d79fe79f3af79e3c4b1c9a1fe6a5d53426
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-03-28 21:32:38 +0200

    update package.json

---
+++ LaserAfzar.Web/DataContext/ContactUsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LaserAfzar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LaserAfzar.Web.DataContext
+{
+    public class ContactUsDbContext : DbContext
+    {
+        public ContactUsDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<ContactUs> ContactUses { get; set; }
+    }
+}
