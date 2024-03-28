b864bca0b246e622d9eae4a33047b34aa1c67773
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-03-28 21:32:38 +0200

    update to latest version

---
+++ LaserAfzar.Web/DataContext/CompanyServiceDbContext.cs
@@ -0,0 +1,22 @@
+﻿using LaserAfzar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LaserAfzar.Web.DataContext
+{
+    public class CompanyServiceDbContext : DbContext
+    {
+        public CompanyServiceDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<CompanyService> CompanyServices { get; set; }
+        public DbSet<ServiceTitleImage> ServiceTitleImages { get; set; }
+        public DbSet<SubService> SubServices { get; set; }
+        public DbSet<ServiceImage> ServiceImages { get; set; }
+    }
+}
