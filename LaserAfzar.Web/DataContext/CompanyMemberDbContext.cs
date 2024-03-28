6b630eaa72f58a924ab9c3d4ed142562be6bf962
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-03-28 21:32:38 +0200

    improve performance

---
+++ LaserAfzar.Web/DataContext/CompanyMemberDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LaserAfzar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LaserAfzar.Web.DataContext
+{
+    public class CompanyMemberDbContext : DbContext
+    {
+        public CompanyMemberDbContext() : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<CompanyMember> CompanyMembers { get; set; }
+        public DbSet<CompanyMemberImage> CompanyMemberImages { get; set; }
+    }
+}
