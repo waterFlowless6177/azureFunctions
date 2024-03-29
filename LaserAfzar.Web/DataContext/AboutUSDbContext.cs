1bf60956b74380a715def307bcfe5f56584a2d71
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-03-29 21:32:38 +0200

    update css

---
+++ LaserAfzar.Web/DataContext/AboutUSDbContext.cs
@@ -0,0 +1,19 @@
+﻿using LaserAfzar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LaserAfzar.Web.DataContext
+{
+    public class AboutUSDbContext : DbContext
+    {
+        public AboutUSDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<AboutUs> AboutUses { get; set; }
+    }
+}
