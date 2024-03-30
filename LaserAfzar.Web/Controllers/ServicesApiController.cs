efefe24cd0b7502df48c5b40ee3645e1f0a4a4a0
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-03-30 21:32:38 +0200

    Fix build errors

---
+++ LaserAfzar.Web/Controllers/ServicesApiController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using LaserAfzar.Entities;
+using LaserAfzar.Web.DataContext;
+
+namespace LaserAfzar.Web.Controllers
+{
+    public class ServicesApiController : ApiController
+    {
+        private CompanyServiceDbContext db = new CompanyServiceDbContext();
+
+        public ServicesApiController()
+        {
+            db.Configuration.ProxyCreationEnabled = false;
+        }
+
+
+        // GET: api/ServicesApi
+        public IQueryable<CompanyService> GetCompanyServices()
+        {
+            return db.CompanyServices.Include(c => c.ServiceTitleImage);
+        }
+
+        // GET: api/ServicesApi/5
+        [ResponseType(typeof(CompanyService))]
+        public async Task<IHttpActionResult> GetCompanyService(int id)
+        {
+            CompanyService companyService = await db.CompanyServices.Include(c => c.ServiceTitleImage)
+                                                                    .FirstOrDefaultAsync(e => e.CompanyServiceId == id);
+            if (companyService == null)
+            {
+                return NotFound();
+            }
+
+            companyService.SubServices = db.SubServices.Include(i => i.ServiceImage)
+                                                       .Where(s => s.CompanyService.CompanyServiceId == id)
+                                                       .ToList();
+
+            return Ok(companyService);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CompanyServiceExists(int id)
+        {
+            return db.CompanyServices.Count(e => e.CompanyServiceId == id) > 0;
+        }
+    }
+}
