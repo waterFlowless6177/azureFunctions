d133f3d03304e59df3f83b5a8dd3e27466b8f4ae
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-03-30 21:32:38 +0200

    improve performance

---
+++ LaserAfzar.Web/Controllers/ServicesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LaserAfzar.Web.Controllers
+{
+    public class ServicesController : Controller
+    {
+        // GET: Services
+        public ActionResult Details(int id)
+        {
+            ViewBag.serviceId = id;
+            return View();
+        }
+    }
+}
