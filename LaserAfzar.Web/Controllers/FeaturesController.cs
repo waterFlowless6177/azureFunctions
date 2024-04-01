618d0b9b7677626aad850b9c79b27e92d1ed2463
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-04-01 21:32:38 +0300

    Fix layout issue

---
+++ LaserAfzar.Web/Controllers/FeaturesController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LaserAfzar.Entities;
+using LaserAfzar.Web.DataContext;
+
+namespace LaserAfzar.Web.Controllers
+{
+    [Authorize]
+    public class FeaturesController : Controller
+    {
+        private FeaturesDbContext db = new FeaturesDbContext();
+
+        // GET: Features
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Features.ToListAsync());
+        }
+
+        // GET: Features/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Feature feature = await db.Features.FindAsync(id);
+            if (feature == null)
+            {
+                return HttpNotFound();
+            }
+            return View(feature);
+        }
+
+        // GET: Features/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Features/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "FeatureId,Title,Description,Icon")] Feature feature)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Features.Add(feature);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(feature);
+        }
+
+        // GET: Features/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Feature feature = await db.Features.FindAsync(id);
+            if (feature == null)
+            {
+                return HttpNotFound();
+            }
+            return View(feature);
+        }
+
+        // POST: Features/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "FeatureId,Title,Description,Icon")] Feature feature)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(feature).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(feature);
+        }
+
+        // GET: Features/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Feature feature = await db.Features.FindAsync(id);
+            if (feature == null)
+            {
+                return HttpNotFound();
+            }
+            return View(feature);
+        }
+
+        // POST: Features/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Feature feature = await db.Features.FindAsync(id);
+            db.Features.Remove(feature);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
