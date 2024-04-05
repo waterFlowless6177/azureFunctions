4d9cc0928b23244a16090b66837162ab79a4420a
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-04-05 21:32:38 +0300

    add user settings

---
+++ LaserAfzar.Web/Controllers/AboutUsApiController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using LaserAfzar.Entities;
+using LaserAfzar.Web.DataContext;
+
+namespace LaserAfzar.Web.Controllers
+{
+    public class AboutUsApiController : ApiController
+    {
+        private AboutUSDbContext db = new AboutUSDbContext();
+
+        // GET: api/AboutUsApi
+        public IQueryable<AboutUs> GetAboutUses()
+        {
+            return db.AboutUses;
+        }
+
+        // GET: api/AboutUsApi/5
+        [ResponseType(typeof(AboutUs))]
+        public async Task<IHttpActionResult> GetAboutUs(int id)
+        {
+            AboutUs aboutUs = await db.AboutUses.FindAsync(id);
+            if (aboutUs == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(aboutUs);
+        }
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AboutUsExists(int id)
+        {
+            return db.AboutUses.Count(e => e.AboutUsId == id) > 0;
+        }
+    }
+}
