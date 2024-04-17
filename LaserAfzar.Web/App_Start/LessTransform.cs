f510b08abb1a800b2cf0a6e19cd30574029e3745
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-04-17 21:32:38 +0300

    Updated API endpoints

---
+++ LaserAfzar.Web/App_Start/LessTransform.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using dotless.Core;
+using System.IO;
+
+namespace LaserAfzar.Web.App_Start
+{
+    public class LessTransform : IBundleTransform
+    {
+        private string _path;
+
+        public LessTransform(string path)
+        {
+            _path = path;
+        }
+
+        public void Process(BundleContext context, BundleResponse response)
+        {
+            Directory.SetCurrentDirectory(_path);
+
+            response.Content = Less.Parse(response.Content);
+            response.ContentType = "text/css";
+        }
+    }
+}
