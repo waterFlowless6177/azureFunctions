a04b263bb92138a510c79e364da45bcb836cb365
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-04-27 21:32:38 +0300

    Update dependencies

---
+++ LaserAfzar.Entities/ServiceTitleImage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace LaserAfzar.Entities
+{
+    [DataContract]
+    public class ServiceTitleImage : ProjectImage
+    {
+        [ForeignKey("CompanyService")]
+        [DataMember]
+        public int ServiceTitleImageId { get; set; }
+
+        public virtual CompanyService CompanyService { get; set; }
+    }
+}
