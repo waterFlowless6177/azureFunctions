9cbe535a323bc5ac3984a2885374b82ea1b6da79
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-04-27 21:32:38 +0300

    Fix issues with deployment scripts

---
+++ LaserAfzar.Entities/ServiceImage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace LaserAfzar.Entities
+{
+    [DataContract]
+    public class ServiceImage : ProjectImage
+    {
+        [DataMember]
+        public int ServiceImageId { get; set; }
+
+        public virtual SubService SubService { get; set; }
+    }
+}
