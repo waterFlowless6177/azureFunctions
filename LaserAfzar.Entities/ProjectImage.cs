d33a524f76b54d8e5e793b60946b38c30f0ec0e0
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-04-28 21:32:38 +0300

    update docs

---
+++ LaserAfzar.Entities/ProjectImage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace LaserAfzar.Entities
+{
+    [DataContract]
+    public class ProjectImage
+    {
+        [Required]
+        [StringLength(255)]
+        [DataMember]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(255)]
+        [DataMember]
+        public string FileName { get; set; }
+    }
+}
