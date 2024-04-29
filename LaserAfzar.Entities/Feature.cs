65efdb998e3c69d255a8675d84abcc4599eb07f1
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-04-29 21:32:38 +0300

    update environment settings

---
+++ LaserAfzar.Entities/Feature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaserAfzar.Entities
+{
+    public class Feature
+    {
+        public int FeatureId { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(1000)]
+        public string Description { get; set; }
+        [Required]
+        [StringLength(255)]
+        [Display(Name ="FontAwesome Icon")]
+        public string Icon { get; set; }
+    }
+}
