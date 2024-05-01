b811942e61176924fd21caf11ff448c454537db2
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-05-01 21:32:38 +0300

    fix layout issue

---
+++ LaserAfzar.Entities/CompanyService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace LaserAfzar.Entities
+{
+    public class CompanyService
+    {
+        public int CompanyServiceId { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+        [Required]
+        [StringLength(1000)]
+        public string BriefDescription { get; set; }
+        [Required]
+        [StringLength(5000)]
+        [AllowHtml]
+        public string FullDescription { get; set; }
+
+        public virtual ServiceTitleImage ServiceTitleImage { get; set; }
+        public virtual ICollection<SubService> SubServices { get; set; }
+
+    }
+}
