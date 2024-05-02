fdc3b047cfcf52f3d9d62631e33cb800fdacf741
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-05-02 21:32:38 +0300

    fixed css issues

---
+++ LaserAfzar.Entities/CompanyMemberImage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace LaserAfzar.Entities
+{
+    [DataContract]
+    public class CompanyMemberImage : ProjectImage
+    {
+        [ForeignKey("CompanyMember")]
+        [DataMember]
+        public int CompanyMemberImageId { get; set; }
+
+        public virtual CompanyMember CompanyMember { get; set; }
+    }
+}
