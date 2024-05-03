b6d3efd8ba77d69708044fe9a55ce7caf07ea803
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-05-03 21:32:38 +0300

    improve performance

---
+++ LaserAfzar.Entities/CompanyMember.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaserAfzar.Entities
+{
+    public class CompanyMember
+    {
+        public int CompanyMemberID { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string JobTitle { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Message { get; set; }
+        
+        public virtual CompanyMemberImage CompanyMemberImage { get; set; }
+    }
+}
