28dc2edca2771a2edbfddc6c730bb441515af8e7
waterFlowless6177 <waterFlowless6177@tutamail.com>
2024-05-03 21:32:38 +0300

    fix layout issue

---
+++ LaserAfzar.Entities/AboutUs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace LaserAfzar.Entities
+{
+    public class AboutUs
+    {
+        public int AboutUsId { get; set; }
+        [Required]
+        [StringLength(5000)]
+        [AllowHtml]
+        public string Description { get; set; }
+    }
+}
